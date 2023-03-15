21463421c40d83092f888289a17e902ce5acd331
KIRK SAHDO <ksahdo10@gmail.com>
2023-03-15 17:38:42 -0400

    feat: add styled-components lib and themes

---
+++ src/themes/index.js
@@ -0,0 +1,17 @@
+export const lightTheme = {
+  primaryColor: '#6403b5',
+  backgroundColor: '#f7f7f7',
+  textColor: '#1f1f1f',
+  lightGrayColor: '#a6a6a6',
+  secondaryColor: '#f2f2f2',
+  disabledColor: '#d9d9d9',
+};
+
+export const darkTheme = {
+  primaryColor: '#6403b5',
+  backgroundColor: '#1f1f1f',
+  textColor: '#f7f7f7',
+  lightGrayColor: '#a6a6a6',
+  secondaryColor: '#f2f2f2',
+  disabledColor: '#d9d9d9',
+};
